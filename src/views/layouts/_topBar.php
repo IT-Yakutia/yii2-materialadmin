ff5884e5ad42bf395ce7f6ad017c52b1779f4a2c
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-04-27 21:48:04 +0900

    add breadcrumbs

--- src/views/layouts/_topBar.php
+++ src/views/layouts/_topBar.php
@@ -2,13 +2,31 @@
 
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
 
 ?>
 <header>
-	<nav>
-        <div class="nav-wrapper">
-        	<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            <a href="#" class="brand-logo truncate"><img src="<?= $adminBundle->baseUrl; ?>/img/logo.png" alt="admin icon" height="15"> <?= $this->title; ?></a>
+    <nav>
+        <div class="row">
+            <div class="col s12">
+        	    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <?= Breadcrumbs::widget([
+                    'tag' => false,
+                    'itemTemplate' => "{link}",
+                    'activeItemTemplate' => '<a class="breadcrumb truncate">{link}</a>',
+                    'homeLink' => [
+                        'label' => '<img src="' . $adminBundle->baseUrl . '/img/logo.png" alt="admin icon" height="15">',
+                        'url' => Url::home(),
+                        'encode' => false,
+                        'class' => ' hide-on-med-and-down tooltipped',
+                        'data-position' => "bottom",
+                        'data-tooltip' => "Главная",
+                    ],
+                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [
+                        $this->title,
+                    ],
+                ]); ?>
+            </div>
         </div>
     </nav>
 </header>
