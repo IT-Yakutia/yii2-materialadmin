9dc48f3c7a4e2ebff64ab62d1df4ca0ea5115a5b
Ayaal Kaplin <uraankhayayaal@gmail.com>
2023-02-26 13:23:47 +0900

    add lazyload js

--- src/assets/src/js/admin.js
+++ src/assets/src/js/admin.js
@@ -1,14 +1,15 @@
 (function($){
     $(function(){
   
-      $('.sidenav').sidenav();
-      $('select').formSelect();
-      $('.collapsible').collapsible();
-      $('.tabs').tabs();
-      $('.modal').modal();
-      $('.materialboxed').materialbox();
-      $('.tooltipped').tooltip();
+        $('.sidenav').sidenav();
+        $('select').formSelect();
+        $('.collapsible').collapsible();
+        $('.tabs').tabs();
+        $('.modal').modal();
+        $('.materialboxed').materialbox();
+        $('.tooltipped').tooltip();
+        $('.lazy').lazy();
   
     }); // end of document ready
-  })(jQuery); // end of jQuery name space
+})(jQuery); // end of jQuery name space
   
